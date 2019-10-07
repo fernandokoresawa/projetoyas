09e178400ae8d867466eae3e4798338fa1a4471b
Fernando Koresawa <fgkm96@gmail.com>
2019-10-07 11:56:36 -0300

    dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,9 +1,119 @@
-<tbody>
+<!--     Fonts and icons     -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<!-- CSS Files -->
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/now-ui-dashboard.css?v=1.3.0') }}" rel="stylesheet" />
+<!-- CSS Just for demo purpose, don't include it in your project -->
+<link href="{{ asset('assets/demo/demo.css') }}" rel="stylesheet" />
+
+
+<?php
+$contStatusT = 0;
+$contStatusF = 0;
+?>
 @foreach($historicos as $historico)
-    <tr>
-        <td>{{ $historico->status }}</td><br>
-        <td>{{ $historico->data }}</td>
-        
-    </tr>
+<?php
+if ($historico->status == 1) {
+  $contStatusT++;
+} else if ($historico->status == 0) {
+  $contStatusF++;
+}
+
+?>
 @endforeach
-</tbody>
+
+
+<div class="wrapper ">
+
+  <div class="main-panel" id="main-panel">
+    <div class="panel-header panel-header-lg">
+      <canvas id="bigDashboardChart"></canvas>
+    </div>
+  </div>
+</div>
+
+<!--   Core JS Files   -->
+<script src="{{ asset('assets/js/core/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+<!--  Google Maps Plugin    -->
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- Chart JS -->
+<script src="{{ asset('assets/js/plugins/chartjs.min.js') }}"></script>
+<!--  Notifications Plugin    -->
+<script src="{{ asset('assets/js/plugins/bootstrap-notify.js') }}"></script>
+<!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+<script src="{{ asset('assets/js/now-ui-dashboard.min.js?v=1.3.0') }}" type="text/javascript"></script>
+<!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+<script src="{{ asset('assets/demo/demo.js') }}"></script>
+<script>
+  $(document).ready(function() {
+    // Javascript method's body can be found in assets/js/demos.js
+    demo.initDashboardPageCharts(<?php echo $contStatusT ?>, <?php echo $contStatusF ?>);
+
+  });
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" } ],
+        ["True", <?php echo $contStatusT ?>, "green"],
+        ["False", <?php echo $contStatusF ?>, "red"],
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Histórico de sensores!",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+      chart.draw(view, options);
+  }
+  </script>
+<div id="columnchart_values" style="width: 900px; height: 300px;"></div>
+
+-->
